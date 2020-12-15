660748f0236f80cd3966381ebaa31ea623abfa23
blueC4P <osamag4g@gmail.com>
2020-12-15 22:28:33 +0100

    8th kata
    Added 8th kata to the repo

---
+++ codewars/javascript/8kyu-Hello-Name-or-World!/index.js
@@ -0,0 +1,19 @@
+function hello(name) {
+    var capitalLetter = "";
+    var lowerLetter = "";
+      
+      if (typeof(name) != "string" || name == ""){
+        return "Hello, World!";
+      }else{
+        for (i in name){
+          if ( i == 0){
+              capitalLetter = name.charAt(i).toUpperCase();
+          }else{
+              lowerLetter = lowerLetter + name.charAt(i).toLowerCase();
+          }
+          i++;
+        }
+        name = capitalLetter + lowerLetter;
+        return 'Hello, ' + name +"!";
+      }
+}
